3fedac5febfac6d979d76f8aadc47edf7c12b6c8
priti1985 <43487027+priti1985@users.noreply.github.com>
2019-03-27 15:28:23 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
 //   API_URL: 'localhost:5005/api'
-  API_URL: 'https://secure-hamlet-12892.herokuapp.com/api/'
+  API_URL: 'http://secure-hamlet-12892.herokuapp.com/api'
 };
